1b5ee4745da41c2742405fa6413fd4d59a6332a2
cosmos <paulchristian946@gmail.com>
2023-07-25 19:04:13 +0800

    feat: notes app

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
-const notes = ["Test", "Test1", "Test2"];
+import { ref } from "vue";
+
+const newNote = ref("");
+
+type Note = {
+  note: string;
+  done: boolean;
+  date: string;
+};
+
+const notes = ref<Array<Note>>([]);
+
+function removeNote(note: Object) {
+  notes.value = notes.value.filter((t) => t !== note);
+}
 </script>
 
 <template>
-  <div class="text-2xl" v-for="note in notes">{{ note }}</div>
+  <input
+    v-model="newNote"
+    @keyup.enter="
+      () => {
+        notes.push({ note: newNote, done: false, date: Date() });
+        newNote = '';
+      }
+    "
+  />
+  <button
+    @click="
+      () => {
+        notes.push({ note: newNote, done: false, date: Date() });
+        newNote = '';
+      }
+    "
+  >
+    Test
+  </button>
+  <div class="text-2xl flex flex-row" v-for="(note, index) in notes">
+    <p v-bind:id="`${index}`">{{ index + 1 }} {{ note.note }}</p>
+    <button @click="removeNote(note)">X</button>
+  </div>
 </template>
