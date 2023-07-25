328b53a3a826fbc440eadf24a720f211acfe7c11
cosmos <paulchristian946@gmail.com>
2023-07-25 13:50:48 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Notes from "./components/Notes.vue";
+</script>
+
+<template>
+  <Notes />
+</template>
+
+<style>
+@import "./style.css";
+</style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const notes = ["Test", "Test1", "Test2"];
+</script>
+
+<template>
+  <div class="text-2xl" v-for="note in notes">{{ note }}</div>
+</template>
